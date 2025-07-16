c828dd4aa24b45ba869fe183588fb1f3a3cc3e04
hasharas <hasharamsankalpam@gmail.com>
2025-07-17 01:18:09 +0530

    Style:home page

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
       return (
@@ -7,6 +8,28 @@ const Home = () => {
                   <div className='home-content'>
                         <div className='home-content-left'>
                               <h3 className=''>Add Task</h3>
+                              <form className='form' action="">
+                                    <div>
+                                          <p>Title</p>
+                                          <input
+                                                type="text"
+                                                placeholder='e.g., Buy groceries'
+                                                required />
+                                    </div>
+
+                                    <div>
+                                          <p>Description</p>
+                                          <input
+                                                type="text"
+                                                placeholder='e.g., Milk, eggs, bread for the week'
+                                                required />
+                                    </div>
+                                    <button>
+                                          Add Task
+                                    </button>
+                              </form>
+
+
                         </div>
                         <div className='home-content-right'>
                               <h3 className=''>Recent Tasks</h3>
