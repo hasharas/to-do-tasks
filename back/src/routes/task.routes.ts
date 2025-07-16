db4fe55358e4a1888e6a4b69273ecdf4ed951c8e
hasharas <hasharamsankalpam@gmail.com>
2025-07-16 11:54:13 +0530

    Create:taskservice

---
+++ back/src/routes/task.routes.ts
@@ -0,0 +1,9 @@
+import {Router} from "express";
+import * as TaskController from "../controllers/task.controller";
+
+const router = Router();
+
+
+router.post('/tasks', TaskController.createTask);
+
+export default router;
--- back/src/services/task.service.ts
+++ back/src/services/task.service.ts
@@ -7,3 +7,13 @@ export const createTask = async (title: string, description: string) => {
       const task = repo.create({title, description});
       return await repo.save(task);
 };
+
+export const getAllTasks = async () => {
+      return await repo.find({
+            where: {isCompleted: false},
+            order: {
+                  createdAt: "DESC"
+            },
+            take: 5
+      });
+}
