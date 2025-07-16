336b0628741853553ee2add78db47c4e5024b28c
hasharas <hasharamsankalpam@gmail.com>
2025-07-16 13:10:54 +0530

    Create:env file

--- back/src/db/data-source.ts
+++ back/src/db/data-source.ts
@@ -4,10 +4,10 @@ import {Task} from "../entities/task.entity";
 //databace connection
 export const TaskDataSource = new DataSource({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1111',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD,
       database: 'tasks2_db',
       synchronize: true,
       logging: false,
